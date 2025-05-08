5e8b77e2019073e0545242b703ec61a8779f840f
Thodoris-Evangelakos <thodorisevangelakos@gmail.com>
2025-05-08 17:58:35 +0300

    move dealer policy from dealer.py to the more appropriate policies.py

--- src/blackjack_solver_simple/agents/policies.py
+++ src/blackjack_solver_simple/agents/policies.py
@@ -5,6 +5,17 @@ from typing import Tuple
 from blackjack_solver_simple.core.players.base import Policy
 from blackjack_solver_simple.core.state import BJState
 
+class DealerPolicy(Policy):
+    def decide(self, state: BJState) -> str:
+        total = state.dealer_total
+        soft = state.dealer_soft  # noqa F401
+        assert total is not None, "Dealer's total should be revealed/filled in env"
+
+        if total <= 16:
+            return "hit"
+        else:
+            return "stand"
+
 
 class RandomPolicy(Policy):
 
--- src/blackjack_solver_simple/core/players/dealer.py
+++ src/blackjack_solver_simple/core/players/dealer.py
@@ -6,24 +6,13 @@ from blackjack_solver_simple.core.hand import Hand
 from blackjack_solver_simple.core.deck import Deck
 from blackjack_solver_simple.core.card import Card, Rank
 from blackjack_solver_simple.core.state import BJState
-
-
-class _DealerPolicy:
-    def decide(self, state: BJState) -> str:
-        total = state.dealer_total
-        soft = state.dealer_soft  # noqa F401
-        assert total is not None, "Dealer's total should be revealed/filled in env"
-
-        if total <= 16:
-            return "hit"
-        else:
-            return "stand"
+from blackjack_solver_simple.agents.policies import DealerPolicy
 
 
 class Dealer(Player):
 
     def __init__(self) -> None:
-        super().__init__(_DealerPolicy())
+        super().__init__(DealerPolicy())
         self.hole_card: Card | None = None  # set/revealed by env
 
     def play(self, deck: Deck, add_visible_count: Callable[[Card], None]) -> None:
