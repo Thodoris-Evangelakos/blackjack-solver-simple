4ceb51a76ba9d83155b1588b0efbdda3f586612d
HarryHoulis <83029946+HarryHoulis@users.noreply.github.com>
2025-05-11 16:58:07 +0300

    implement TabularQPolicy

--- src/blackjack_solver_simple/agents/policies.py
+++ src/blackjack_solver_simple/agents/policies.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 import random
 from typing import Tuple
-
+from collections import defaultdict
+import numpy as np
 from blackjack_solver_simple.core.players.base import Policy
 from blackjack_solver_simple.core.state import BJState
 
@@ -23,6 +24,37 @@ class RandomPolicy(Policy):
         return random.choice(("hit", "stand"))
 
 
-class TabularQPolicy:
-    # TODO
-    ...
+
+class TabularQPolicy(Policy):
+
+    def __init__(self, env:Env,learning_rate: float,initial_epsilon: float,gamma: float):
+        self.env = env
+        self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+
+        self.alpha = learning_rate
+        self.gamma = gamma
+
+        self.epsilon = initial_epsilon
+        self.training_error_qlearning = []
+        self.training_error_sarsa = []
+
+
+    def decide(self, state: BJState):
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        else:
+            return int(np.argmax(self.q_values[BJState]))
+            
+    def _update(self, state: BJState, action: int, reward: float,terminated:int,next_state: tuple[int, int, int]) -> None:
+        if terminated:
+            future_q_value = 0.0
+        else:
+            future_q_value = np.max(self.q_values[next_state])
+
+        temporal_difference = (reward + self.gamma * future_q_value - self.q_values[state][action] )
+
+        self.q_values[state][action] = (self.q_values[state][action] + self.alpha * temporal_difference)
+
+        #represents the difference between the current Q‑value estimate and the “target” value computed from the reward and the estimated future Q‑values
+        self.training_error_qlearning.append(temporal_difference)
+    
